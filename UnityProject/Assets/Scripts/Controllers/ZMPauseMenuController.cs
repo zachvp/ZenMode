519c629e1f8f981e6af987998551a842fe8fc6fb
Zach Vega-Perkins <vegaperk@usc.edu>
2015-04-12 17:13:33 -0700

    Implemented pause menu selection

--- UnityProject/Assets/Scripts/Controllers/ZMPauseMenuController.cs
+++ UnityProject/Assets/Scripts/Controllers/ZMPauseMenuController.cs
@@ -1,20 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 public class ZMPauseMenuController : MonoBehaviour {
+	public Text[] menuOptions;
+
 	private bool _active;
+	private int  _selectedIndex;
+	private int  _optionsSize;
+
+	private Color _baseColor;
+	private Color _selectedColor;
 
 	void Awake() {
+		_selectedIndex = 0;
+		_baseColor 	   = menuOptions[0].color;
+		_selectedColor = new Color(255, 255, 255, 255);
+
 		ZMGameStateController.PauseGameEvent += HandlePauseGameEvent;
 		ZMGameStateController.ResumeGameEvent += HandleResumeGameEvent;
 
 		ToggleActive(false);
+
+		_optionsSize = menuOptions.GetLength(0);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			HandleMenuNavigationForward();
+		} else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			HandleMenuNavigationBackward();
+		}
 	}
 
 	void HandlePauseGameEvent ()
 	{
 		Debug.Log("Pause");
+
+		_selectedIndex = 0;
+
 		ToggleActive(true);
+		UpdateUI();
 	}
 
 	void HandleResumeGameEvent() {
@@ -22,8 +47,36 @@ public class ZMPauseMenuController : MonoBehaviour {
 		ToggleActive(false);
 	}
 
+	void HandleMenuNavigationForward() {
+		_selectedIndex += 1;
+		_selectedIndex %= _optionsSize;
+
+		UpdateUI();
+	}
+
+	void HandleMenuNavigationBackward() {
+		_selectedIndex -= 1;
+		_selectedIndex = _selectedIndex < 0 ? _optionsSize - 1 : _selectedIndex;
+
+		UpdateUI();
+	}
+
+	private void ToggleSelection(int index, bool selected) {
+		menuOptions[index].color = selected ? _selectedColor : _baseColor;
+	}
+
 	private void ToggleActive(bool active) {
 		_active = active;
 		gameObject.SetActive(_active);
+	}
+
+	private void UpdateUI() {
+		for (int i = 0; i < _optionsSize; ++i) {
+			if (i != _selectedIndex) {
+				ToggleSelection(i, false);
+			}
+		}
+		
+		ToggleSelection(_selectedIndex, true);
 	}
 }
