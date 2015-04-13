717c157fc90a7b67f631d0c1a1940fbe8e552ef6
Zach Vega-Perkins <vegaperk@usc.edu>
2015-04-12 23:28:24 -0700

    Made breaking blocks juicier

--- UnityProject/Assets/Scripts/Environment/ZMBreakable.cs
+++ UnityProject/Assets/Scripts/Environment/ZMBreakable.cs
@@ -2,8 +2,20 @@
 using System.Collections;
 
 public class ZMBreakable : MonoBehaviour {
-
+	public ParticleSystem particleSystem;
+	
 	public void HandleCollision() {
+		particleSystem.transform.position = transform.position;
+		particleSystem.Play();
+		Invoke ("StopGibs", 0.1f);
+
+		gameObject.SetActive(false);
+		//renderer.enabled = false;
+		//collider2D.enabled = false;
+	}
+
+	void StopGibs() {
+		particleSystem.Stop();
 		Destroy(gameObject);
 	}
 }
