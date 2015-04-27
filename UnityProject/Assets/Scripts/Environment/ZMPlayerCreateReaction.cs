2a9d36641b159307fdd156eeabdf1994b2a03a2e
Zach Vega-Perkins <vegaperk@usc.edu>
2015-04-27 02:15:20 -0700

    Added player reaction script

---
+++ UnityProject/Assets/Scripts/Environment/ZMPlayerCreateReaction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using ZMPlayer;
+
+public class ZMPlayerCreateReaction : MonoBehaviour {
+	public bool activateOnJoin = true;
+
+	ZMPlayerInfo _playerInfo;
+
+	void Awake() {
+		_playerInfo = GetComponent<ZMPlayerInfo>();
+		ZMPlayerController.PlayerCreateEvent += HandlePlayerCreateEvent;
+	}
+
+	void HandlePlayerCreateEvent (ZMPlayerController playerController)
+	{
+		if ((int) _playerInfo.playerTag > ZMPlayerManager.NumPlayers - 1 && gameObject != null)
+			Destroy(gameObject);
+	}	
+}
