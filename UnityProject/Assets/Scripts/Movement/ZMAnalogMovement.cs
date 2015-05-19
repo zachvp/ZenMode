148926106d984dc9b8782fceb11dc78e27f77aa1
zachvp <vegaperk@usc.edu>
2015-05-19 04:37:42 -0600

    Added barriers to prevent orbs from leaving the field

--- UnityProject/Assets/Scripts/Movement/ZMAnalogMovement.cs
+++ UnityProject/Assets/Scripts/Movement/ZMAnalogMovement.cs
@@ -3,13 +3,15 @@ using InControl;
 using ZMPlayer;
 
 public class ZMAnalogMovement : MonoBehaviour {
-	public float _movementSpeed = 4;
+	public float _movementSpeed = 100;
 
 	private Vector3 _deltaPos;
+	private Vector3 _forward;
+	private bool _shouldBounce;
 
 	// references
 	private ZMPlayerInfo _playerInfo;
-	private int _controlIndex;
+	private int _controlIndex;	
 
 	void Awake () {
 		_playerInfo = GetComponent<ZMPlayerInfo>();
@@ -22,11 +24,31 @@ public class ZMAnalogMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		_deltaPos = transform.position;
+		if (!_shouldBounce) {
+			_deltaPos = transform.position;
+			_forward =  new Vector3(_deltaPos.x, _deltaPos.y, _deltaPos.z);
 
-		_deltaPos.x += InputManager.Devices[_controlIndex].LeftStickX * _movementSpeed;
-		_deltaPos.y += InputManager.Devices[_controlIndex].LeftStickY * _movementSpeed;
+			_deltaPos.x += InputManager.Devices[_controlIndex].LeftStickX;
+			_deltaPos.y += InputManager.Devices[_controlIndex].LeftStickY;
 
-		transform.position = _deltaPos;
+			_forward = _deltaPos - _forward;
+
+			rigidbody2D.velocity = _forward * _movementSpeed;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.layer.Equals(LayerMask.NameToLayer("Barrier"))) {
+			Vector2 bounceVector = new Vector2(_forward.x, _forward.y);
+			bounceVector *= -1 * 600;
+
+			rigidbody2D.velocity = bounceVector;
+			_shouldBounce = true;
+			Invoke("CancelBounce", 0.2f);
+		}
+	}
+
+	void CancelBounce() {
+		_shouldBounce = false;
 	}
 }
