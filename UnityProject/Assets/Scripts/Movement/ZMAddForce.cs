094deb74c85b459d49a7aea8128fa7da111d9428
zachvp <vegaperk@usc.edu>
2015-05-31 18:07:55 -0700

    Began logic for despawning body parts

--- UnityProject/Assets/Scripts/Movement/ZMAddForce.cs
+++ UnityProject/Assets/Scripts/Movement/ZMAddForce.cs
@@ -9,11 +9,15 @@ public class ZMAddForce : MonoBehaviour {
 	private float _sprayRate;
 
 	private const float DISSAPATE_RATE = 20;
-	private const float MIN_SPRAY_RATE = 4;
+	private const float MIN_SPRAY_RATE = 2;
+	private const float LIFETIME = 3f;
 
 	private static float BaseEmissionRate = 0;
+	private static int InstanceCount = 0;
 
 	void Start () {
+		InstanceCount += 1;
+
 		rigidbody2D.AddForce(force);
 		rigidbody2D.AddTorque(torque);
 
@@ -25,6 +29,8 @@ public class ZMAddForce : MonoBehaviour {
 
 		_sprayRate = BaseEmissionRate;
 		_familyParticleSystem.renderer.material.color = _particleColor;
+
+		Invoke ("Despawn", LIFETIME);
 	}
 
 	void Update() {
@@ -34,6 +40,13 @@ public class ZMAddForce : MonoBehaviour {
 
 			_familyParticleSystem.renderer.material.color = _particleColor;
 			_familyParticleSystem.startColor = _particleColor;
+		}
+	}
+
+	void Despawn() {
+		if (InstanceCount > 0) {
+			InstanceCount -= 1;
+			Destroy(gameObject);
 		}
 	}
 
