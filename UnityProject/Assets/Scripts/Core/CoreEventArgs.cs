b0e924aac763244ca55d160e78a2e51d1ee6f503
zachvp <vegaperk@usc.edu>
2016-07-10 20:36:44 -0600

    Added necessary EventArgs classes to ZMCore and Core.

---
+++ UnityProject/Assets/Scripts/Core/CoreEventArgs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+	public class IntEventArgs : EventArgs
+	{
+		public int value;
+	}
+
+	public class UnityObjectEventArgs : EventArgs
+	{
+		public UnityEngine.Object arg;
+	}
+
+	public class MonoBehaviourEventArgs : EventArgs
+	{
+		public MonoBehaviour behavior;
+	}
+
+	public class MonoBehaviourIntEventArgs : MonoBehaviourEventArgs
+	{
+		public int value;
+	}
+
+	public class MonoBehaviourFloatEventArgs : MonoBehaviourEventArgs
+	{
+		public float value;
+	}
+
+	public class LayerEventArgs : EventArgs
+	{
+		public LayerEventArgs layer;
+	}
+}
---
+++ UnityProject/Assets/Scripts/ZMCore/ZMEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class ZMPlayerInfoEventArgs : EventArgs
+{
+	public ZMPlayerInfo info;
+}
+
+public class ZMPlayerInfoInputEventArgs : ZMPlayerInfoEventArgs
+{
+	public ZMInput input;
+}
+
+public class ZMInputEventArgs : EventArgs
+{
+	public ZMInput input;
+}
+
+public class ZMInputVector2EventArgs : ZMInputEventArgs
+{
+	public Vector2 value;
+}
+
+public class ZMInputFloatEventArgs : ZMInputEventArgs
+{
+	public float value;
+}
+
+public class ZMMenuOptionEventArgs : EventArgs
+{
+	public ZMMenuOption option;
+}
